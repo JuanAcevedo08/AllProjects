21f81b629c7e70cff4ea69626467d5fea75b7c55
Juan Acevedo <juandavidacevedon@gmail.com>
2025-04-01 11:37:16 -0500

    Create Word Extractor

---
+++ Short Exercise/Extraccion.py
@@ -0,0 +1,6 @@
+Sentence = input("Which is your sentence? ")
+Start = int(input("Which is your beginning? "))
+Finish = int(input("Which is yout end "))
+Word = Sentence[Start:Finish]
+
+print("You Wor Is... :", Word)
