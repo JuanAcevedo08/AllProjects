63177a6d49ba83fc8d00fcc4c16d531d95d98570
Juan Acevedo <juandavidacevedon@gmail.com>
2025-04-07 15:57:26 -0500

    Add Short JOB

---
+++ Short Exercise/KnowTheDatasWithExtraccion.py
@@ -0,0 +1,10 @@
+#I WILL HELP A PRINCIPAL TO READ STUDENT INFORMATION MORE EASILY
+
+Student_Data = ["Names: ", "Juan", "Jose", "Ashy", "Zul", "Luisa"], ["Ages: ", 16, 17, 16, 19, 17], ["Nationalities: ", "Colombia", "Venezuela", "United States", "Ecuador", "Colombia"]
+
+NameData = int(input("If you want to know the names, press 0 "))
+print(Student_Data[NameData])
+AgeData = int(input("If you want to know the ages, press 1 "))
+print(Student_Data[AgeData])
+NationalityData = int(input("If you want to know the nationalities, press 2 "))
+print(Student_Data[NationalityData])
