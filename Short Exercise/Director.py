a4452e6611727c86334709b8e674809cc2b6d141
Juan Acevedo <juandavidacevedon@gmail.com>
2025-04-09 10:48:36 -0500

    market

---
+++ Short Exercise/Director.py
@@ -0,0 +1,17 @@
+estudiantes = {
+   "Ana": {
+"Apellidos": "Sanchez Fernanez", "Edad": 17, "Tiempo en la escuela ": "9 meses" 
+    },
+    "Juan": {
+"Apellidos": "Sanchez Fernanez", "Edad": 17, "Tiempo en la escuela ": "9 meses" 
+    },
+    "Steve": {
+"Apellidos": "Sanchez Fernanez", "Edad": 17, "Tiempo en la escuela ": "9 meses" 
+    },
+    "": {
+"Apellidos": "Sanchez Fernanez", "Edad": 17, "Tiempo en la escuela ": "9 meses" 
+    },
+    "Ana": {
+"Apellidos": "Sanchez Fernanez", "Edad": 17, "Tiempo en la escuela ": "9 meses" 
+    }
+}
